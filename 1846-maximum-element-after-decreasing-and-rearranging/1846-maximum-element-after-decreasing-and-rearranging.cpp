ba568561cfa1a15217df62b1d5b0b77bb7621460
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-06-28 08:31:47 +0530

    Time: 11 ms (41.24%), Space: 55.1 MB (94.35%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -5,7 +5,12 @@ public:
         sort(arr.begin(), arr.end());
         arr[0] = 1;
         for (int i = 1; i < n; i++) {
-            arr[i] = min(arr[i], arr[i - 1] + 1);
+            //either this
+            // arr[i] = min(arr[i], arr[i - 1] + 1);
+            //or this
+            if(arr[i]-arr[i-1]>1){
+                arr[i]=arr[i-1]+1;
+            }
         }
         return (arr[n - 1]);
     }
