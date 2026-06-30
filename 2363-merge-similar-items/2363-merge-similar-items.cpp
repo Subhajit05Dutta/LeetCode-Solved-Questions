79d3c64a7704272b4067d630818124b31f3eaecd
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-06-30 20:57:59 +0530

    Time: 6 ms (59.67%), Space: 20.9 MB (70.8%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.cpp
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1,
-                                          vector<vector<int>>& items2) {
+     vector<vector<int>>& items2) {
         map<int, int> mp;
         for (auto& it : items1) {
             mp[it[0]] += it[1];
