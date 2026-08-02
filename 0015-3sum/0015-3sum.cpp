cfad44317dedaf4fddc4e676393c4b1e0cc758ac
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-02 17:54:31 +0530

    Time: 46 ms (68.11%), Space: 29 MB (73.54%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
+
         for (int i = 0; i < n - 2; i++) {
-            // This line is the key to avoiding duplicate triplets.
-            if (i > 0 && nums[i] == nums[i - 1]) {
+            if (i > 0 && (nums[i - 1] == nums[i])) {
                 continue;
             }
             int l = i + 1;
             int r = n - 1;
-            int need = (-nums[i]);
 
             while (l < r) {
-
-                if ((nums[l] + nums[r]) == need) {
-                    vector<int> vec = {nums[i], nums[l], nums[r]};
-                    ans.push_back(vec);
+                long long sum = nums[l] + nums[r];
+                if (sum + nums[i] == 0) {
+                    ans.push_back({nums[i], nums[l], nums[r]});
                     l++;
                     r--;
-                    while (l < r && nums[l] == nums[l - 1]) {
+                    while (l < r && nums[l - 1] == nums[l]) {
                         l++;
                     }
                     while (l < r && nums[r] == nums[r + 1]) {
                         r--;
                     }
-                } else if ((nums[l] + nums[r]) < need) {
-                    l++;
-                } else {
+                } else if (sum + nums[i] > 0) {
                     r--;
+                } else if (sum + nums[i] < 0) {
+                    l++;
                 }
             }
-            
         }
         return ans;
     }
