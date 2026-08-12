87bb992beb59086fec748b1e0945de7a3385b987
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-12 11:42:11 +0530

    Time: 0 ms (100%), Space: 52.5 MB (25.85%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int max_idx=0;
+        for(int i=0;i<n;i++){
+            if(i>max_idx){
+                return false;
+            }
+            max_idx=max(max_idx,i+nums[i]);
+            if(max_idx>=n){
+                return true;
+            }
+        }
+        return true;
+    }
+};
