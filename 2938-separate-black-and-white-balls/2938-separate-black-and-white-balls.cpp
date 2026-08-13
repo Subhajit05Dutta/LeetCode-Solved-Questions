64ccf2b3ab15837cfd3647fcd6ea2cedf842100f
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-13 13:29:03 +0530

    Time: 8 ms (38.87%), Space: 17.3 MB (9.31%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        long long cnt=0;
-        int last=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]-0=='0'){
-                cnt+=(i-last);
+        long long cnt = 0;
+        int last = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '0') {
+                cnt += (i - last);
                 last++;
             }
         }
