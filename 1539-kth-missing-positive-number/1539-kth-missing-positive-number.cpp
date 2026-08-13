3b79d153381c3c8ca9c0a7278e30d3b24c2f488b
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-13 11:11:14 +0530

    Time: 0 ms (100%), Space: 13.3 MB (29.71%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -2,6 +2,25 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         int n = arr.size();
+        int low = 0, high = n;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int missing = arr[mid] - (mid + 1);
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low + k;
+    }
+};
+
+/*
+    //Alternative Approach with O(n) time complexity
+
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
         int st = 1;
         int i = 0;
         int ans = 0;
@@ -23,4 +42,4 @@ public:
         }
         return ans;
     }
-};
+*/
