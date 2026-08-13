34a41023cceacf6fe3ef67d4c5e795be476e694f
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-13 13:28:28 +0530

    Time: 7 ms (52.28%), Space: 17.2 MB (43.98%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long cnt=0;
+        int last=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]-0=='0'){
+                cnt+=(i-last);
+                last++;
+            }
+        }
+        return cnt;
+    }
+};
