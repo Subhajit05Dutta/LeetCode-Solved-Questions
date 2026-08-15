455a8b3da4f9870046578715f3d401a953c1079e
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-15 15:07:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.01%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int total(int n, unordered_map<int, int>& dp) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (dp.find(n) == dp.end()) {
+            dp[n] = total(n - 1, dp) + total(n - 2, dp);
+        }
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        unordered_map<int, int> dp;
+        return total(n, dp);
+    }
+};
