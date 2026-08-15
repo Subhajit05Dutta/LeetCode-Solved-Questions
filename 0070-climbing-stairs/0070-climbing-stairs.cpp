33a2d378b25ec1e21e7c8d0d337bb13f831aada5
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-15 19:26:01 +0530

    Time: 0 ms (100%), Space: 8.4 MB (56.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
-    int total(int n, unordered_map<int, int>& dp) {
-        if (n == 0 || n == 1) {
+    unordered_map<int,int>dp;
+    int fun(int i,int n){
+        if(i==n){
             return 1;
         }
-        if (dp.find(n) == dp.end()) {
-            dp[n] = total(n - 1, dp) + total(n - 2, dp);
+        if(i>n){
+            return 0;
         }
-        return dp[n];
+
+        if(dp.find(i)!=dp.end()){
+            return dp[i];
+        }
+        int a1=fun(i+1,n);
+        int a2=fun(i+2,n);
+        dp[i]=a1+a2;
+        return dp[i];
     }
     int climbStairs(int n) {
-        unordered_map<int, int> dp;
-        return total(n, dp);
+        int total= fun(0,n);
+         return total;
     }
 };
+
+/*
+// int total(int n, unordered_map<int, int>& dp) {
+    //     if (n == 0 || n == 1) {
+    //         return 1;
+    //     }
+    //     if (dp.find(n) == dp.end()) {
+    //         dp[n] = total(n - 1, dp) + total(n - 2, dp);
+    //     }
+    //     return dp[n];
+    // }
+    // int climbStairs(int n) {
+    //     unordered_map<int, int> dp;
+    //     return total(n, dp);
+    // }
+*/
