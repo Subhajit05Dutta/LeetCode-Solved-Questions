619e2b3e0d1c2ab83b1fe1c6bc90bf9a2f4b3126
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-16 19:52:29 +0530

    Time: 7 ms (52.17%), Space: 13.5 MB (6.13%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> mp = {{'I', 1},   {'V', 5},   {'X', 10},
-                                       {'L', 50},  {'C', 100}, {'D', 500},
-                                       {'M', 1000}};
-        int res = 0;
-        int i = 0, j = 1;
-        while(j<s.length()){
-            if(mp[s[i]]<mp[s[j]]){
-                res=res+(mp[s[j]]-mp[s[i]]);
-                i=i+2;
-                j=j+2;
-            }
-            else{
-                res=res+mp[s[i]];
+        unordered_map<char, int> mpp = {{'I', 1},   {'V', 5},   {'X', 10},
+                                        {'L', 50},  {'C', 100}, {'D', 500},
+                                        {'M', 1000}};
+        int n = s.size();
+        int ans = 0;
+        int i = 0;
+        while (i < n) {
+            if (i == n) {
+                ans = ans + mpp[s[i]];
                 i++;
-                j++;
+            } else if (mpp[s[i]] < mpp[s[i + 1]]) {
+                ans = ans + (mpp[s[i + 1]] - mpp[s[i]]);
+                i = i + 2;
+            } else {
+                ans = ans + mpp[s[i]];
+                i++;
             }
         }
-        // If last character is left
-        if(i<s.length()){
-            res+=mp[s[i]];
-            i++;
-        }
-        return res;
+        return ans;
     }
 };
-
