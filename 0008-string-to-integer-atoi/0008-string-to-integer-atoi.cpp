1980a779f9bdc4283f11751f7be56a0a525ac9ba
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-16 20:07:47 +0530

    Time: 0 ms (100%), Space: 9.2 MB (19.95%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,69 +1,33 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int n=s.size();
-        int i=0;
-        long long res=0;
-        while(s[i]==' '){
+        int n = s.size();
+        int sign = 1;
+        long long ans = 0;
+        int i = 0;
+        while (i < n && s[i] == ' ') {
             i++;
         }
-        if(i==n){
-            return 0;
+        if (i == n) {
+            return ans;
         }
-        int sign=1;
-        if(s[i]=='-'){
-            sign=-1;
-            i++;
-        }
-        else if(s[i]=='+'){
+        if ((s[i] == '+') || (s[i] == '-')) {
+            if (s[i] == '-')
+                sign = -1;
             i++;
         }
 
-        while(i<n&&isdigit(s[i])){
-            int num=(int)(s[i]-'0');
-            res=res*10+num;
-            if(sign*res<INT_MIN){
+        while ((i < n) && (isdigit(s[i]))) {
+            int digit = s[i] - '0';
+            ans = ans * 10 + digit;
+
+            if (sign * ans > INT_MAX) {
+                return INT_MAX;
+            } else if (sign * ans < INT_MIN) {
                 return INT_MIN;
             }
-            if(sign*res>INT_MAX){
-                return INT_MAX;
-            }
             i++;
         }
-        return (int)sign*res;
+        return (sign * ans);
     }
 };
-/*
-//Alternative Approach
-        int myAtoi(string s) {
-            long res = 0;
-            int sign = 1;
-            int i = 0;
-            while (i < s.length() && s[i] == ' ') {
-                i++;
-            }
-            if (s[i] == s.length()) {
-                return 0;
-            }
-            if (s[i] == '-') {
-                sign = -1;
-                i++;
-            } else if (s[i] == '+') {
-                i++;
-            }
-
-            while (i < s.length() &&   isdigit(s[i]) ) {
-                res = res * 10 + (s[i] - '0');
-                if (res * sign > INT_MAX) {
-                    return INT_MAX;
-                }
-                if (res * sign < INT_MIN) {
-                    return INT_MIN;
-                }
-
-                i++;
-            }
-            return (int)(sign * res);
-        }
-*/
-
