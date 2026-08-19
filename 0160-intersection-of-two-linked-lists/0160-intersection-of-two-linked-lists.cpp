5ca64425eb3e2644fb94d603e005952461072cce
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-19 16:19:57 +0530

    Time: 51 ms (84.04%), Space: 23.8 MB (99.15%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,51 +8,27 @@
  */
 class Solution {
 public:
-    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-        ListNode* temp1 = headA;
-        ListNode* temp2 = headB;
-        if (temp1 == NULL || temp2 == NULL) {
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        ListNode* first=headA;
+        ListNode* second=headB;
+        if(first==NULL || second==NULL){
             return NULL;
         }
-        // Ultimately temp1 has to cover the same distance which temp2 has
-        // cover.so basically they are covering the same amount of distance
-        // together.
-        // It is 3+2=2+3 kind of thing
-        while (temp1 != temp2) {
-            temp1 = temp1->next;
-            temp2 = temp2->next;
-            if (temp1 == temp2) {
+        while(first!=second){
+            
+            first=first->next;
+            second=second->next;
+            if(first==second){
                 break;
             }
-            if (temp1 == NULL) {
-                temp1 = headB;
+            if(first==NULL){
+                first=headB;
             }
-            if (temp2 == NULL) {
-                temp2 = headA;
+            if(second==NULL){
+                second=headA;
             }
         }
-        return temp1;
+        return  first;
     }
 };
-
-/*
-//Alternative Approach
-
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp1=headA;
-        ListNode* temp2=headB;
-        unordered_map<ListNode*,int>mpp;
-        while(temp1!=NULL){
-            mpp[temp1]=1;
-            temp1=temp1->next;
-        }
-        while(temp2!=NULL){
-            if(mpp.count(temp2)==1){
-                return temp2;
-            }
-            mpp[temp2]=1;
-            temp2=temp2->next;
-        }
-        return NULL;
-    }
-*/
