f81e021fb8dd6e261f0907c251e3c7d009818391
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-22 20:45:14 +0530

    Time: 4 ms (0.98%), Space: 10.3 MB (5.11%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    vector<string> ans;
-    void fun(string& s, int idx, int n, string& dairy, vector<string>& ans,
+    void fun(string& s, int i, int n, string& dairy, vector<string>& ans,
              unordered_map<char, string> mp) {
         // Base Case:
         // If we have processed all digits,
         // store the current combination.
-        if (idx == n) {
+        if (i == n) {
             ans.push_back(dairy);
             return;
         }
         // Get all letters corresponding to the current digit
-        string choice = mp[s[idx]];
+        string choice = mp[s[i]];
         // Try every possible letter for the current digit
         for (int j = 0; j < choice.size(); j++) {
-            // Choice
             dairy.push_back(choice[j]);
             // Explore the next digit
-            fun(s, idx + 1, n, dairy, ans, mp);
+            fun(s, i + 1, n, dairy, ans, mp);
             // Backtrack:
             // Remove the last chosen character
             // so that another choice can be tried.
             dairy.pop_back();
         }
+
         return;
     }
     vector<string> letterCombinations(string digits) {
@@ -33,7 +32,9 @@ public:
         unordered_map<char, string> mp = {
             {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
             {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+
         int n = digits.size();
+        vector<string> ans;
         string dairy = "";
         fun(digits, 0, n, dairy, ans, mp);
         return ans;
