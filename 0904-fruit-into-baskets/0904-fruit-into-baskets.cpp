b78addfb7023804a29838383ae7041c9470bf1c2
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-23 14:17:29 +0530

    Time: 47 ms (43.42%), Space: 84.9 MB (34.51%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int freq(unordered_map<int,int>&mpp){
-        int total=0;
-        for(auto &it:mpp){
-            total+=it.second;
+    int freq(unordered_map<int, int>& mpp) {
+        int total = 0;
+        for (auto& it : mpp) {
+            total += it.second;
         }
         return total;
     }
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int>mpp;
-        int n=fruits.size();
-        int l=0,r=0;
-        int cnt=0,maxcnt=0;
-        while(r<n){
+        unordered_map<int, int> mpp;
+        int n = fruits.size();
+        int l = 0, r = 0;
+        int cnt = 0, maxcnt = 0;
+        while (r < n) {
             mpp[fruits[r]]++;
-            while(mpp.size()>2){
+            while (mpp.size() > 2) {
                 mpp[fruits[l]]--;
-                if(mpp[fruits[l]]==0){
+                if (mpp[fruits[l]] == 0) {
                     mpp.erase(fruits[l]);
                 }
                 l++;
             }
-            cnt=freq(mpp);
-            maxcnt=max(cnt,maxcnt);
+            cnt = freq(mpp);
+            maxcnt = max(cnt, maxcnt);
             r++;
         }
         return maxcnt;
