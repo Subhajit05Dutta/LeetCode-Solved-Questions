89a0b4f9eaf880a5cc30b6bdf3836973a50aa2ec
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-26 12:33:12 +0530

    Time: 0 ms (100%), Space: 12 MB (31.59%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    int fun(vector<vector<int>>& grid, vector<vector<int>>& dp, int i, int j) {
-        if (i >= 0 && j >= 0 && grid[i][j] == 1) { // Dead Cell
-            return 0;
-        }
-        if (i == 0 && j == 0) {
-            return 1;
-        }
-        if (i < 0 || j < 0) {
-            return 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        int up = fun(grid, dp, i - 1, j);
-        int left = fun(grid, dp, i, j - 1);
-        return dp[i][j] = up + left;
-    }
+    // Memoization(Bottom-Up Approach)
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return fun(grid, dp, m - 1, n - 1);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i == 0 && j == 0) {
+                    dp[0][0] = 1;
+                } else if (i < 0 || j < 0) {
+                    return 0;
+                } else {
+                    int up = 0;
+                    int left = 0;
+                    if (i > 0) {
+                        up = dp[i - 1][j];
+                    }
+                    if (j > 0) {
+                        left = dp[i][j - 1];
+                    }
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
 
@@ -47,5 +54,29 @@ int fun(vector<vector<int>>& grid, int i, int j) {
         return fun(grid, m - 1, n - 1);
     }
 
+//Memoization(Top-Down Approach)
+int fun(vector<vector<int>>& grid, vector<vector<int>>& dp, int i, int j) {
+        if (i >= 0 && j >= 0 && grid[i][j] == 1) { // Dead Cell
+            return 0;
+        }
+        if (i == 0 && j == 0) {
+            return 1;
+        }
+        if (i < 0 || j < 0) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int up = fun(grid, dp, i - 1, j);
+        int left = fun(grid, dp, i, j - 1);
+        return dp[i][j] = up + left;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return fun(grid, dp, m - 1, n - 1);
+    }
 
 */
