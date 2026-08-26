ff96648b4d1543012c8cf445c830d35ecbb6a83a
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-26 11:27:08 +0530

    Time: 0 ms (100%), Space: 9.4 MB (54.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int fun(vector<vector<int>>& dp, int i, int j) {
-        if (i == 0 && j == 0) {
-            return 1;
-        }
-        if (i < 0 || j < 0) { // out of boundary
-            return 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        int up = fun(dp, i - 1, j);
-        int left = fun(dp, i, j - 1);
-        return dp[i][j] = up + left;
-    }
+    // Memoization(Bottom-Up Approach)
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return fun(dp, m - 1, n - 1);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[0][0] = 1;
+                } else {
+                    int up = 0;
+                    if (i > 0)
+                        up = dp[i - 1][j];
+                    int left = 0;
+                    if (j > 0)
+                        left = dp[i][j - 1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
 
@@ -37,4 +39,25 @@ int fun(int i, int j) {
         return fun(m-1, n-1);
     }
 
+
+
+//Memoization (Top-Down Approach)
+    int fun(vector<vector<int>>& dp, int i, int j) {
+        if (i == 0 && j == 0) {
+            return 1;
+        }
+        if (i < 0 || j < 0) { // out of boundary
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int up = fun(dp, i - 1, j);
+        int left = fun(dp, i, j - 1);
+        return dp[i][j] = up + left;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return fun(dp, m - 1, n - 1);
+    }
 */
