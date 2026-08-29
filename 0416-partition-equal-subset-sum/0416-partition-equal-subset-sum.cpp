0b5a5d914132321245953163aa4c62feda3f726c
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-08-29 12:19:41 +0530

    Time: 151 ms (37.41%), Space: 17.4 MB (67.45%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,5 @@
 class Solution {
 public:
-    bool fun(vector<int>& nums,vector<vector<int>>&dp, int idx, int target) {
-        if (target == 0) {
-            return true;
-        }
-        if (idx == 0) {
-            return (target == nums[0]);
-        }
-        if(dp[idx][target]!=-1){
-            return dp[idx][target];
-        }
-        bool take = false;
-        if (target >= nums[idx]) {
-            take = fun(nums,dp, idx - 1, target - nums[idx]);
-        }
-
-        bool not_take = fun(nums, dp,idx - 1, target);
-
-        return dp[idx][target]=(take || not_take);
-    }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int totsum = 0;
@@ -29,11 +10,32 @@ public:
             // Total sum is odd so it can't be divided into 2 equal parts.
             return false;
         }
-        vector<vector<int>>dp(n,vector<int>(totsum/2+1,-1));
-        return fun(nums,dp, n - 1, totsum / 2);
+        vector<vector<bool>> dp(n, vector<bool>(totsum / 2 + 1, false));
+        int target = totsum / 2;
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        if (nums[0] <= target) {
+            dp[0][nums[0]] = true;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= target; j++) {
+
+                // Not Take
+                bool not_take = dp[i - 1][j];
+
+                // Take
+                bool take = false;
+                if (j >= nums[i]) {
+                    take = dp[i - 1][j - nums[i]];
+                }
+                dp[i][j] = (take || not_take);
+            }
+        }
+        return dp[n - 1][target];
     }
 };
-
 
 /*
 //Recursive Approach
@@ -65,5 +67,39 @@ public:
             return false;
         }
         return fun(nums, n - 1, totsum / 2);
+    }
+
+//Memoization (top-Down Approach)
+    bool fun(vector<int>& nums, vector<vector<int>>& dp, int idx, int target) {
+        if (target == 0) {
+            return true;
+        }
+        if (idx == 0) {
+            return (target == nums[0]);
+        }
+        if (dp[idx][target] != -1) {
+            return dp[idx][target];
+        }
+        bool take = false;
+        if (target >= nums[idx]) {
+            take = fun(nums, dp, idx - 1, target - nums[idx]);
+        }
+
+        bool not_take = fun(nums, dp, idx - 1, target);
+
+        return dp[idx][target] = (take || not_take);
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int totsum = 0;
+        for (int i = 0; i < n; i++) {
+            totsum += nums[i];
+        }
+        if (totsum % 2 == 1) {
+            // Total sum is odd so it can't be divided into 2 equal parts.
+            return false;
+        }
+        vector<vector<int>> dp(n, vector<int>(totsum / 2 + 1, -1));
+        return fun(nums, dp, n - 1, totsum / 2);
     }
 */
