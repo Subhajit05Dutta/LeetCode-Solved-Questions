08402427d0def9b5a37656bd5a6eff89145a867d
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-09-02 09:30:47 +0530

    Time: 0 ms (100%), Space: 30.3 MB (67.8%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n=nums1.size();
-        int odd=0,even=0;
-        for(int i:nums1){
-            if(i%2==0){
-                even++;
-            }
-            else{
-                odd++;
-            }
-        }
-        //It's always doable. return 1
-        if(even==n || odd==n){
-            return true;
-        }
-        /*
-        If the elements of nums1 are all even or all odd, just set nums2=nums1
-        Otherwise there must exist some odd x in nums1, use z=y-x for all even y in nums1, & z=y for all odd y in nums1 to make all z odd.
-        */
         return true;
+        // int n = nums1.size();
+        // int odd = 0, even = 0;
+        // for (int i : nums1) {
+        //     if (i % 2 == 0) {
+        //         even++;
+        //     } else {
+        //         odd++;
+        //     }
+        // }
+        // // It's always doable. return 1
+        // if (even == n || odd == n) {
+        //     return true;
+        // }
+        // /*
+        // If the elements of nums1 are all even or all odd, just set nums2=nums1
+        // Otherwise there must exist some odd x in nums1, use z=y-x for all even y
+        // in nums1, & z=y for all odd y in nums1 to make all z odd.
+        // */
+        // return true;
     }
 };
