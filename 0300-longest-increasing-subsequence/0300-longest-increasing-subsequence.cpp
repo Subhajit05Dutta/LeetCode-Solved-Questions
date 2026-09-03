4c426cc6bc8eb8af85ef9cc64cf33694c8db8ce0
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-09-03 12:03:31 +0530

    Time: 75 ms (49.65%), Space: 14.5 MB (48.37%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int fun(vector<int>& nums, int i, int n, int prev,
-            vector<vector<int>>& dp) {
-        if (i == n) {
-            return 0;
-        }
-        if (dp[i][prev + 1] != -1) {
-            return dp[i][prev + 1];
-        }
-        if (prev == -1 || nums[i] > nums[prev]) {
-            int c1 = 1 + fun(nums, i + 1, n, i, dp);    // take nums[i]
-            int c2 = 0 + fun(nums, i + 1, n, prev, dp); // skip nums[i]
-            return dp[i][prev + 1] = max(c1, c2);
-        }
-        return dp[i][prev + 1] =
-                   fun(nums, i + 1, n, prev,
-                       dp); // we have to skip nums[i], there is no choice.
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return fun(nums, 0, n, -1, dp);
+        vector<int> res(n);
+        for (int i = 0; i < n; i++) {
+            res[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    res[i] = max(res[i], res[j] + 1);
+                }
+            }
+        }
+        int maxi = 0;
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, res[i]);
+        }
+        return maxi;
     }
 };
 
@@ -41,4 +36,28 @@ public:
         int n=nums.size();
         return fun(nums,0,n,-1);
     }
+
+
+    //Memoization (Top-Down Approach)
+    int fun(vector<int>& nums, int i, int n, int prev,
+            vector<vector<int>>& dp) {
+        if (i == n) {
+            return 0;
+        }
+        if (dp[i][prev + 1] != -1) {
+            return dp[i][prev + 1];
+        }
+        if (prev == -1 || nums[i] > nums[prev]) {
+            int c1 = 1 + fun(nums, i + 1, n, i, dp);    // take nums[i]
+            int c2 = 0 + fun(nums, i + 1, n, prev, dp); // skip nums[i]
+            return dp[i][prev + 1] = max(c1, c2);
+        }
+        return dp[i][prev + 1] =
+                   fun(nums, i + 1, n, prev,
+                       dp); // we have to skip nums[i], there is no choice.
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        retu
 */
