d1b2f9ac34072cbaf345f26954276247e6c6ce9d
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-09-06 20:55:47 +0530

    Time: 0 ms (100%), Space: 31.3 MB (81.23%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,13 +4,13 @@ public:
         int n = nums.size();
         int low = 0, high = n - 1;
         while (low <= high) {
-            int mid = low + (high - low) / 2;
+            int mid = (low + high) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (target < nums[mid]) {
-                high = mid - 1;
-            } else {
+            } else if (nums[mid] < target) {
                 low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return -1;
