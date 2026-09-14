dd6c2cdeda2f370adc0f8d80c115ba6b7fc9b6a2
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-09-14 21:12:26 +0530

    Time: 3 ms (16.84%), Space: 16 MB (54.11%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -6,35 +6,37 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(root==NULL){
+        vector<vector<int>> ans;
+        if (root == NULL) {
             return ans;
         }
-        queue<TreeNode*>q;
+        queue<TreeNode*> q;
         q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            vector<int>level;
-            for(int i=0;i<size;i++){
-                TreeNode* node=q.front();
-                q.pop();
+        while (!q.empty()) {
+            vector<int> level;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
                 level.push_back(node->val);
-                if(node->left!=NULL){
+                q.pop();
+
+                if (node->left != NULL) {
                     q.push(node->left);
                 }
-                if(node->right!=NULL){
+                if (node->right != NULL) {
                     q.push(node->right);
                 }
             }
             ans.push_back(level);
         }
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
