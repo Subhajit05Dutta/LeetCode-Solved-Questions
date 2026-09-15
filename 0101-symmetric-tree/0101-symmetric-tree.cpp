c6d2d3981cc2fa43c9c2a6d523e150a1fa33a3b4
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-09-15 15:00:59 +0530

    Time: 0 ms (100%), Space: 18.3 MB (94.22%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -6,27 +6,29 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool Ismirror(TreeNode* n1, TreeNode* n2) {
-        if (n1 == NULL && n2 == NULL) {
+    bool isMirror(TreeNode* r1,TreeNode* r2){
+        if(r1==NULL && r2==NULL){
             return true;
-        } else if (n1 == NULL || n2 == NULL) {
-            return false;
-        } else if (n1->val != n2->val) {
+        }
+        else if(r1==NULL || r2==NULL){
             return false;
         }
-        return ((n1->val == n2->val) && (Ismirror(n1->left, n2->right) &&
-                                         Ismirror(n1->right, n2->left)));
+        else if(r1->val!=r2->val){
+            return false;
+        }
+        return ((r1->val==r2->val)&& (isMirror(r1->left,r2->right)&&isMirror(r1->right,r2->left)));
+
     }
     bool isSymmetric(TreeNode* root) {
-        if (root == NULL) {
+        if(root==NULL){
             return true;
         }
-        return Ismirror(root->left, root->right);
+        
+        return isMirror(root->left,root->right);
     }
 };
