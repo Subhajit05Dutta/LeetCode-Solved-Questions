f9906faf9dafe7e1c0f5a278c861d612afc19271
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-09-17 19:30:33 +0530

    Time: 0 ms (100%), Space: 9 MB (60.87%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        int freq[26] = {0};
+        int freq[26]={0};
         /*
         Frequency Count + Greedy
         Count all characters.
@@ -9,10 +9,10 @@ public:
         use freq['l']/2 and freq['o']/2.
         The minimum available count among (b, a, l/2, o/2, n) is the answer.
         */
-        for (char c : text) {
-            freq[c - 'a']++;
+        for(int i=0;i<text.size();i++){
+            freq[text[i]-'a']++;
         }
-        return (min({freq[0], freq[1], freq['l' - 'a'] /2,
-                     freq['o' - 'a']/2, freq['n' - 'a']}));
+
+        return min({freq[0],freq[1],freq['l'-'a']/2,freq['o'-'a']/2,freq['n'-'a']});
     }
 };
