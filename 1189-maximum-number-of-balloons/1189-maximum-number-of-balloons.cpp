0614615f53d6c47ae02ae20e7122581e309f0113
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-09-17 19:27:51 +0530

    Time: 4 ms (3.37%), Space: 9 MB (60.87%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int freq[26] = {0};
+        /*
+        Frequency Count + Greedy
+        Count all characters.
+        Since 'l' and 'o' appear twice in "balloon",
+        use freq['l']/2 and freq['o']/2.
+        The minimum available count among (b, a, l/2, o/2, n) is the answer.
+        */
+        for (char c : text) {
+            freq[c - 'a']++;
+        }
+        return (min({freq[0], freq[1], freq['l' - 'a'] /2,
+                     freq['o' - 'a']/2, freq['n' - 'a']}));
+    }
+};
