47dd46e22e6183ddfb0d006628fb8d419d064405
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-09-19 12:32:27 +0530

    Time: 0 ms (100%), Space: 22.2 MB (27.13%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -16,8 +16,14 @@ public:
         if (root == NULL) {
             return 0;
         }
-        int lh = maxDepth(root->left);
-        int rh = maxDepth(root->right);
-        return 1 + max(lh, rh);
+
+        if (root->right == NULL) {
+            return 1 + maxDepth(root->left);
+        }
+        if (root->left == NULL) {
+            return 1 + maxDepth(root->right);
+        }
+
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
