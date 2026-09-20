49444793468a4056933f0631447666f73c9034b7
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-09-20 19:46:11 +0530

    Time: 0 ms (100%), Space: 9.8 MB (15.78%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        int sum=0;
-        int i=0;
-        while(i<s.size()){
-            int cal=(i+1)*(97-((int)(s[i])) +26 );
-            sum=sum+cal;
+        int sum = 0;
+        int i = 0;
+        while (i < s.size()) {
+            // int cal = (i + 1) * (97 - ((int)(s[i])) + 26);
+            int cal = (i + 1) * (26 - (s[i] - 'a'));
+            sum = sum + cal;
             i++;
         }
         return sum;
