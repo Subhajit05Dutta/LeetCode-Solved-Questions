5ef95bd48f54f56593740779f463f4407689b41a
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-09-21 11:19:45 +0530

    Time: 0 ms (100%), Space: 21.4 MB (95.5%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -6,33 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool ans=false;
-    int sum=0;
-    void res_sum(TreeNode* root, int targetSum,int sum){
-        if(root==NULL){
+    bool ans = false;
+   
+    void fun(TreeNode* root, int targetSum, int sum) {
+        if (root == NULL) {
             return;
         }
-        sum+=root->val;
-        if(root->left==NULL && root->right==NULL){
-            if(sum==targetSum){
-                ans=true;
-                return;
+
+        sum += root->val;
+        if (root->left == NULL && root->right == NULL) {
+            if (sum == targetSum) {
+                ans = true;
             }
+            return;
         }
-        res_sum(root->left,targetSum,sum);
-        res_sum(root->right,targetSum,sum);
+        
+        fun(root->left, targetSum, sum);
+        fun(root->right, targetSum, sum);
         return;
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if(root==NULL){
+        if (root == NULL) {
             return false;
         }
-        res_sum(root,targetSum,sum);
+        fun(root, targetSum, 0);
         return ans;
     }
 };
