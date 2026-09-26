4cc75647644f8978c71c53d607b15c12214fd3fe
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-09-26 22:58:39 +0530

    Time: 0 ms (100%), Space: 9.3 MB (24.24%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> mp;
-        for (int i = 0; i < s.size(); i++) {
-            mp[s[i]]++;
+        unordered_map<char, int> mpp;
+        for (char ch : s) {
+            mpp[ch]++;
         }
         char ch;
         for (int i = 0; i < t.size(); i++) {
-            if (mp[t[i]] == 0) {
+            if (mpp[t[i]] == 0) {
                 ch = t[i];
             } else {
-                mp[t[i]]--;
+                mpp[t[i]]--;
             }
         }
         return ch;
